726685cdec2f0ba89c3487b59235e7d4336c9253
jquezada1984 <johnquezadahuayamave@gmail.com>
2024-05-05 20:43:35 -0500

    soldado descansando
    soldado descansando.

--- js/scena/scene1.js
+++ js/scena/scene1.js
@@ -5,11 +5,26 @@ class scene1 extends Phaser.Scene {
     }
  
     preload () {
-        this.load.image("fondo",".//assets/Background.png"); // decimos donde esta la imagen a phaser
-        this.load.spritesheet("jugador",".//assets/sprites/Jugador01.png",{ frameWidth: 50,frameHeight: 55}); // Cargamos la imagen del jugador
+        this.load.image("fondo",".//assets/Background.png"); // decimos donde esta la imagen a phaser65.14
+        this.load.spritesheet("jugador",".//assets/sprites/complet01.png",{ frameWidth: 65.14,frameHeight: 79}); // Cargamos la imagen del jugador
     }
 
     create () {
+        //animaciones
+        this.anims.create({
+            key: "detenido", // nombre de la animacion
+            frames: this.anims.generateFrameNumbers("jugador",{start:0,end:6}),// desde que frame hasta que frame
+            frameRate:7,// velocidad de animacion
+            repeat:-1// si se va a reptir y cuantas veces, al estar en -1 se repite infinito
+        });
+
+        this.anims.create({
+            key: "caminar",
+            frames: this.anims.generateFrameNumbers("jugador",{start:7,end:13}),
+            frameRate:10,
+            repeat:-1
+        });
+
         // Fondo
         let fondo = this.add.image(930,530,"fondo"); // agregamos el fondo a la escena
         fondo.setScale(3.5);//escalamos el fondo
@@ -20,7 +35,7 @@ class scene1 extends Phaser.Scene {
         jugador= this.physics.add.sprite(0,0,"jugador"); //agregamos el jugador a la escena         
         jugador.setScale(3);// escalamos el jugador
         jugador.setSize(30,50); //Tamaño del Jugador
-        jugador.setOffset(20,5);
+        jugador.setOffset(35,18);
         jugador.setCollideWorldBounds(true);// hacemos que colisione con el borde del mundo
         
         //Teclas
@@ -35,8 +50,9 @@ class scene1 extends Phaser.Scene {
              jugador.setVelocityX(-180); // si la tecla derecha esta presionada move al personaje en el eje x con una velocidad de -180
          }else {
              jugador.setVelocityX(0); // si no estan ninguna de estas teclas presionadas su velocidad es 0
+             jugador.anims.play("detenido",true);
          }
-         if (cursors.up.isDown) {  /// si se presiona la tecla arriba y  el personaje esta tocando algo abajo salta
+         if (cursors.up.isDown && jugador.body.touching.down) {  /// si se presiona la tecla arriba y  el personaje esta tocando algo abajo salta
             jugador.setVelocityY(-500);
             }
 
